fa989ce527cb3eb254e1cccc9b3dfbea1fc7fe3e
arc-015 <chavanavinash015@gmail.com>
2022-11-20 20:01:10 +0530

    Added : Welcome to the Cab in Voice Generator

---
+++ src/test/java/com/bridgelabz/CabInVoiceGenerator.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+
+public class CabInVoiceGenerator {
+
+    @BeforeAll
+    public static void displayMethod(){
+        System.out.println("Welcome to the Cab In Voice Generator Program");
+
+    }
+
+    @AfterEach
+     public void afterDisplayMethod(){
+        System.out.println("Test is Pass/Run");
+    }
+}
